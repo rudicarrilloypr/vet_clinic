5d2316245a8fa9ac156f71fe9c01022e33176a25
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-08-21 19:47:18 -0600

    Uodate table

--- schema.sql
+++ schema.sql
@@ -6,7 +6,9 @@ CREATE TABLE animals (
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
-    weight_kg DECIMAL(5,2)
+    weight_kg DECIMAL(5,2),
+    species_id INT REFERENCES species(id),
+    owner_id INT REFERENCES owners(id)
 );
 
 CREATE TABLE species (
@@ -20,7 +22,25 @@ CREATE TABLE owners (
     age INT
 );
 
--- Modificaciones a la tabla existente 'animals'
-ALTER TABLE animals DROP COLUMN IF EXISTS species;
-ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
-ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+-- Crear la tabla 'vets'
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    date_of_graduation DATE
+);
+
+-- Crear la tabla 'specializations' (join table para 'species' y 'vets')
+CREATE TABLE specializations (
+    vet_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id),
+    PRIMARY KEY (vet_id, species_id)
+);
+
+-- Crear la tabla 'visits' (join table para 'animals' y 'vets')
+CREATE TABLE visits (
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    visit_date DATE,
+    PRIMARY KEY (animal_id, vet_id, visit_date)
+);
