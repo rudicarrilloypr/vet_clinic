3d13235137a80a5bd97b664cd5435d0f5a33753d
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-08-30 01:55:41 +0200

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,26 +3,25 @@ CREATE TABLE patients (
   name VARCHAR(255),
   date_of_birth DATE
 );
-
 CREATE TABLE medical_histories (
   id SERIAL PRIMARY KEY,
   admited_at TIMESTAMP,
   patient_id INTEGER UNIQUE REFERENCES patients(id),
   email VARCHAR(120)
 );
-
 CREATE TABLE treatments (
   id SERIAL PRIMARY KEY,
   type VARCHAR(100),
   name VARCHAR(255)
 );
-
-
 CREATE TABLE medical_history_treatments (
   medical_history_id INTEGER REFERENCES medical_histories(id),
   treatment_id INTEGER REFERENCES treatments(id),
   PRIMARY KEY (medical_history_id, treatment_id)
 );
+
+CREATE INDEX idx_medical_history_treatments_medical_history_id ON medical_history_treatments (medical_history_id);
+CREATE INDEX idx_medical_history_treatments_treatment_id ON medical_history_treatments (treatment_id);
 
 CREATE TABLE invoice_items (
   id SERIAL PRIMARY KEY,
@@ -32,7 +31,6 @@ CREATE TABLE invoice_items (
   invoice_id INTEGER REFERENCES invoices(id),
   treatment_id INTEGER REFERENCES treatments(id)
 );
-
 CREATE TABLE invoices (
   id SERIAL PRIMARY KEY,
   total_amount DECIMAL,
@@ -40,3 +38,6 @@ CREATE TABLE invoices (
   payed_at TIMESTAMP,
   medical_history_id INTEGER UNIQUE REFERENCES medical_histories(id)
 );
+
+CREATE INDEX idx_invoice_items_invoice_id ON invoice_items (invoice_id);
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items (treatment_id);
